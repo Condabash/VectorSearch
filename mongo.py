4a8a44d5f2443ba98092088efed2136482ece6c9
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-11-11 13:49:56 +0530

    Refactor MongoDB connection to use environment variables for database and collection

--- mongo.py
+++ mongo.py
@@ -4,10 +4,12 @@
 from pymongo import MongoClient
 import env as e
 
-# Get MongoDB URI
+# Get Env Variables
 mongo_uri = e.mongo_uri
+mongo_db = e.mongo_db
+mongo_collection = e.mongo_collection
 
 # Connect to MongoDB
 client = MongoClient(mongo_uri)  # Client
-db = client.sample_mflix  # DB
-collection = db.movies  # Collection
+db = client[mongo_db]  # DB
+collection = db[mongo_collection]  # Collection
