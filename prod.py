953b733884454fb5a8fa20ec8a95a1120866b7cf
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-12-28 14:42:10 +0530

    Update import statement in prod.py to use progenerate module

--- prod.py
+++ prod.py
@@ -1,7 +1,7 @@
 # Production Work #
 
 # Import Packages
-from generate import add_embedding
+from progenerate import add_embedding
 
 # Add Embedding To Collection
 add_embedding()  # Run Only Once
